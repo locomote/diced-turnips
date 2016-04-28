57b89e23326cda8b44be180bff6a683f39d340ed
John Carney <john@carney.id.au>
2016-04-28 22:40:20 +1000

    Minor tweaks to rake tasks

--- lib/diced-turnips/tasks.rb
+++ lib/diced-turnips/tasks.rb
@@ -22,22 +22,21 @@ end
 
 namespace :parallel do
   desc "run turnips in parallel"
-  task "turnips", %i[ count pattern options ] do |t, args|
+  task :turnips, %i[ count pattern options ] do |t, args|
     ParallelTests::Tasks.check_for_pending_migrations
 
     $LOAD_PATH << File.expand_path(File.join(__dir__, ".."))
     require "parallel_tests"
 
-    opts = %w[ -n --pattern --test-options ].zip(DicedTurnips::Tasks.parse_args(args)).reject do |_, v|
-      "#{v}".empty?
-    end
+    opts = %w[ -n --pattern --test-options ].zip(DicedTurnips::Tasks.parse_args(args)).select(&:last).flatten
 
-    executable = File.join(File.dirname(__FILE__), "..", "..", "bin", "parallel_turnip")
+    executable = File.join(__dir__, *%w[ .. .. bin parallel_turnip ])
 
-    command = [ executable, "spec", *opts.flatten ]
+    command = [ executable, "spec", *opts ]
     if ParallelTests::WINDOWS
       command.unshift(File.join(RbConfig::CONFIG["bindir"], RbConfig::CONFIG["ruby_install_name"]))
     end
+
     abort unless system(Shellwords.join(command))
   end
 end
