e105a459f7db116b72ae0c2548bef1aa007905ad
John Carney <jcarney@locomote.com>
2017-10-26 13:43:37 -0600

    Make sure specs at least pass, even if they're not useful

--- spec/diced/turnips_spec.rb
+++ spec/diced/turnips_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Diced::Turnips do
+describe DicedTurnips do
   it 'has a version number' do
-    expect(Diced::Turnips::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(DicedTurnips::VERSION).not_to be nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'diced/turnips'
+require 'diced-turnips'
